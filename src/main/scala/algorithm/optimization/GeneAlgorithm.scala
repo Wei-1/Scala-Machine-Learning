0e073af63ed5f05faf23c0268d0c9e25acd47475
Wei Chen <longlyeagle@gmail.com>
2017-07-22 22:30:22 +0800

    optimize

---
+++ src/main/scala/algorithm/optimization/GeneAlgorithm.scala
@@ -0,0 +1,29 @@
+// Wei Chen - Gene algorithm
+// 2017-07-22
+
+package ght.mi.algorithm
+
+class GeneAlgorithm {
+    var seeds: Array[Array[Double]] = null
+
+    def setSeeds(newseeds: Array[Array[Double]]): Unit = seeds = newseeds
+
+    def evolve(
+        evaluation: Array[Double] => Double,
+        breeding: (Array[Double], Array[Double]) => Array[Double],
+        generationsize: Int = 100,
+        elitesize: Int = 3
+    ): Array[Double] = {
+        val parents = seeds.sortBy { arr =>
+            evaluation(arr)
+        }.takeRight(elitesize)
+        val parentsize = parents.size
+        val kids = (0 until generationsize - parentsize).map { i =>
+            val ai = scala.util.Random.nextInt.abs % parentsize
+            val bi = scala.util.Random.nextInt.abs % parentsize
+            breeding(parents(ai), parents(bi))
+        }
+        seeds = parents ++ kids
+        return parents.last // return the best
+    }
+}
---
+++ src/test/scala/algorithm/optimization/GeneAlgorithmTest.scala
@@ -0,0 +1,41 @@
+// Wei Chen - Bayesian Decision Test
+// 2016-06-03
+
+import org.scalatest.FunSuite
+import ght.mi.general.MatrixFunc._
+import ght.mi.algorithm.GeneAlgorithm
+
+class GeneAlgorithmSuite extends FunSuite {
+
+    val ga = new GeneAlgorithm()
+
+    val initseeds = Array(Array(0.0), Array(1.0))
+
+
+    def evaluation(arr: Array[Double]): Double = - (arr.head - 0.7).abs
+
+    def breeding(pa: Array[Double], pb: Array[Double]): Array[Double] =
+        pa.zip(pb).map { case (a, b) =>
+            if (scala.util.Random.nextDouble > 0.1) (a + b) / 2
+            else scala.util.Random.nextDouble
+        }
+
+    val generationsize: Int = 100
+    val elitesize: Int = 3
+
+    test("GeneAlgorithm Test : Initial") {
+        assert(ga.seeds == null)
+        ga.setSeeds(initseeds)
+        assert(matrixequal(ga.seeds, initseeds))
+    }
+
+    test("GeneAlgorithm Test : Evolve") {
+        for (i <- 0 until 10)
+            ga.evolve(evaluation, breeding, generationsize, elitesize)
+        assert(ga.seeds.size == generationsize)
+
+        val best = ga.evolve(evaluation, breeding, generationsize, elitesize)
+        assert((best.head - 0.7).abs < 0.05)
+    }
+
+}
