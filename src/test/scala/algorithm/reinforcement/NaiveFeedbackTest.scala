4ebe986d82d80013b5342182032bfaa997ead1e2
Wei Chen <longlyeagle@gmail.com>
2017-08-16 11:57:24 +0800

    naive-feedback-test-2

--- src/test/scala/algorithm/reinforcement/NaiveFeedbackTest.scala
+++ src/test/scala/algorithm/reinforcement/NaiveFeedbackTest.scala
@@ -13,22 +13,38 @@ class NaiveFeedbackSuite extends FunSuite {
     val epoch = 100
 
     val statenumber = 5
-    val scores = Map(2 -> 10.0, 3 -> 0.0, 4 -> 100.0)
     val links = Map(0 -> Array(1, 2),
         1 -> Array(3, 4))
 
-    val nf = new NaiveFeedback(statenumber)
-    nf.addScores(scores)
-    nf.addLinks(links)
+    { // Case 1
+        val scores = Map(2 -> 10.0, 3 -> 0.0, 4 -> 100.0)
+        val nf = new NaiveFeedback(statenumber)
+        nf.addScores(scores)
+        nf.addLinks(links)
 
-    test("NaiveFeedback Test : Iterate") {
-        nf.iterate(limit, learning_rate, scale, epoch)
-        assert(true)
+        test("NaiveFeedback Test : Iteration") {
+            nf.iterate(limit, learning_rate, scale, epoch)
+            assert(true)
+        }
+
+        test("NaiveFeedback Test : Result 1") {
+            val result = nf.result(epoch)
+            assert(result.size == 3)
+            assert(result.last.id == 4)
+        }
     }
 
-    test("NaiveFeedback Test : Result") {
-        val result = nf.result(epoch)
-        assert(result.size == 3)
-        assert(result.last.id == 4)
+    { // Case 2
+        val scores = Map(2 -> 10.0, 3 -> 0.0, 4 -> 12.0)
+        val nf = new NaiveFeedback(statenumber)
+        nf.addScores(scores)
+        nf.addLinks(links)
+        nf.iterate(limit, learning_rate, scale, epoch)
+
+        test("NaiveFeedback Test : Result 2 - WRONG") {
+            val result = nf.result(epoch)
+            assert(result.size == 2)
+            assert(result.last.id == 2)
+        }
     }
 }
