aefa6a7467143a3b43a120bb0aae0471317dcd80
Wei Chen <Wei.Chen@tdc.Innovasolutions.int>
2018-09-12 11:25:46 +0800

    classifier-rf

--- src/main/scala/algorithm/classification/Classifier.scala
+++ src/main/scala/algorithm/classification/Classifier.scala
@@ -6,7 +6,9 @@ package com.interplanetarytech.algorithm
 trait Classifier {
     def clear(): Boolean
 
-    def train(data: Array[(Int, Array[Double])], paras: Map[String, Double]): Boolean
+    def config(paras: Map[String, Double]): Boolean
+
+    def train(data: Array[(Int, Array[Double])]): Boolean
 
     def predict(data: Array[Array[Double]]): Array[Int]
 }
